27645f4f2d6267f35efc5354d499a3c0a0cc8a7e
Jin <jin.dev.1425@gmail.com>
2025-06-03 21:01:24 -0400

    first commit

---
+++ Backend/src/test/java/com/github/jinn9/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jinn9.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
